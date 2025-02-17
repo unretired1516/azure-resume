6d75cc6f8de0ffd21ef5a3e2c82633fe4beb328b
Daniel Nance <danieljnance@outlook.com>
2025-02-16 18:20:08 -0600

    Updates  to structure, created main.js file

---
+++ frontend/main.js
@@ -0,0 +1,19 @@
+windows.addEventListener('DOMContentLoaded', (event) => {
+    getVisitCount();
+})
+
+const functionApi = '';
+//Set variable to 0, call the API get the current count, then set the count variable to equal the response. Then display the count in the InnerText.
+const getVisitCount = () => {
+    let count = 0;
+    fetch(functionApi).then(response => {
+        return response.json()
+    }).then(response => {
+        console.log("Website called function API.");
+        count = response.count;
+        document.getElementById("counter").innerText = count;
+    }).catch(function(error) {
+        console.log(error)
+    });
+    return count;
+}
