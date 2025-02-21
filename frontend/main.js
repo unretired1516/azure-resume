57d9b69521dbecb71edd4c6b67ec85b80489a10f
Daniel Nance <danieljnance@outlook.com>
2025-02-20 18:22:43 -0600

    Updated API URL

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,11 @@ windows.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
 })
 const functionApiURL = 'https://getazureresumecounteruniqueidentifierhere.azurewebsites.net/api/VisitorCounter?code=wiNDrGe0UJcCdSuLC18Z90TqErEZVNVThmLsFedWnNK0AzFuWKh58A=='
-const functionApi = 'http://localhost:7071/api/VisitorCounter';
+const localfunctionApi = 'http://localhost:7071/api/VisitorCounter';
 //Set variable to 0, call the API get the current count, then set the count variable to equal the response. Then display the count in the InnerText.
 const getVisitCount = () => {
     let count = 0;
-    fetch(functionApi).then(response => {
+    fetch(functionApiURL).then(response => {
         return response.json()
     }).then(response => {
         console.log("Website called function API.");
