e080f303c44014a5473efd45b4a2918cc5891b14
Daniel Nance <danieljnance@outlook.com>
2025-02-20 19:59:11 -0600

    Refactored main.js code to troubleshoot undefined response returned.

--- frontend/main.js
+++ frontend/main.js
@@ -1,19 +1,20 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
-})
-const functionApiURL = 'https://getazureresumecounteruniqueidentifierhere.azurewebsites.net/api/VisitorCounter?code=wiNDrGe0UJcCdSuLC18Z90TqErEZVNVThmLsFedWnNK0AzFuWKh58A=='
+});
+
+const functionApiURL = 'https://getazureresumecounteruniqueidentifierhere.azurewebsites.net/api/VisitorCounter?code=wiNDrGe0UJcCdSuLC18Z90TqErEZVNVThmLsFedWnNK0AzFuWKh58A==';
 const localfunctionApi = 'http://localhost:7071/api/VisitorCounter';
-//Set variable to 0, call the API get the current count, then set the count variable to equal the response. Then display the count in the InnerText.
-const getVisitCount = () => {
-    let count = 0;
-    fetch(functionApiURL).then(response => {
-        return response.json()
-    }).then(response => {
+
+// Set variable to 0, call the API, get the current count, then set the count variable to equal the response.
+const getVisitCount = async () => {
+    try {
+        const response = await fetch(functionApiURL);
+        const data = await response.json();
         console.log("Website called function API.");
-        count = response.count;
-        document.getElementById("counter").innerText = count;
-    }).catch(function(error) {
-        console.log(error)
-    });
-    return count;
-}
+        // Update the count element on the webpage
+        document.getElementById("counter").innerText = data.count;
+    } catch (error) {
+        console.error('Error fetching the visit count:', error);
+        // Optionally handle error display here
+    }
+};
